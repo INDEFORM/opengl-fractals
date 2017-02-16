b8e2a83e8d38f9da7e3c4083482ab58a2e95a030
idf_algirdasnor <algirdas.noreika@indeform.com>
2017-02-16 23:43:55 +0200

    Initial Fractals classes setup.

--- dev_fractals/js/idfapp/core.js
+++ dev_fractals/js/idfapp/core.js
@@ -2,13 +2,21 @@ IDFAPP.Core = function(p) {
 	this.params = {};
 	this.params['view3d_container'] = ( p.view3d_container ? document.getElementById(p.view3d_container) : null )
 	this.params['events_wrapper'] = ( p.events_wrapper ? document.getElementById(p.events_wrapper) : null )
+	this.current_fractal = null;
 
 	if (this.params.view3d_container != null) {
-		this.view3d = new IDFAPP.View3d(this);		
+		this.view3d = new IDFAPP.View3d(this);
 		this.ui = new IDFAPP.UI(this);
 	}
 }
 
 
 IDFAPP.Core.prototype = {
+	createFractal: function (id, params) {
+		switch(id) {
+	        case 'fractal-builder_aln':
+	            this.current_fractal = new IDFAPP.FractalBuilderAln(this.view3d.scene, params);
+	            break;
+	    }
+	},
 };
---
+++ dev_fractals/js/idfapp/fractal-builder.js
@@ -0,0 +1,8 @@
+IDFAPP.FractalBuilder = function(scene, params) {
+    this.scene = scene;
+    this.params = params;
+}
+
+
+IDFAPP.FractalBuilder.prototype = Object.create(Object.prototype);
+IDFAPP.FractalBuilder.prototype.constructor = IDFAPP.FractalBuilder;
