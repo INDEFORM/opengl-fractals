57c420ff5a2a576cab6913a970cdd2023a7ec39c
idf_audriuskul <audryzas@hotmail.com>
2017-02-17 15:09:34 +0200

    *fractal iteration change

--- dev/js/idfapp/controls.js
+++ dev/js/idfapp/controls.js
@@ -8,22 +8,25 @@ IDFAPP.Controls = function () {
         ArrowRight: false
     };
 
-    this._inverseKey = {
+    this._inverseKeys = {
         ArrowUp: "ArrowDown",
         ArrowDown: "ArrowUp",
         ArrowLeft: "ArrowRight",
         ArrowRight: "ArrowLeft"
     };
 
+    this._actionStack = [];
+
     document.body.addEventListener("keydown", this, false);
     document.body.addEventListener("keyup", this, false);
+    document.body.addEventListener("keypress", this, false);
 };
 
 IDFAPP.Controls.prototype = {
     _handleKeyDown: function (key) {
         if (this._keysState[key] !== undefined) {
-            if (this._inverseKey[key] !== undefined) {
-                this._keysState[key] = this._keysState[this._inverseKey[key]] === false;
+            if (this._inverseKeys[key] !== undefined) {
+                this._keysState[key] = this._keysState[this._inverseKeys[key]] === false;
             } else {
                 this._keysState[key] = true;
             }
@@ -33,6 +36,16 @@ IDFAPP.Controls.prototype = {
         if (this._keysState[key] !== undefined)
             this._keysState[key] = false;
     },
+    _handleKeypress: function (key) {
+        switch (key) {
+            case "+":
+                this._actionStack.push({type: "fractal", action: "increment"});
+                break;
+            case "-":
+                this._actionStack.push({type: "fractal", action: "decrement"});
+                break;
+        }
+    },
     handleEvent: function (evt) {
         switch (evt.type) {
             case "keydown":
@@ -41,9 +54,15 @@ IDFAPP.Controls.prototype = {
             case "keyup":
                 this._handleKeyUp(evt.key);
                 break;
+            case "keypress":
+                this._handleKeypress(evt.key);
+                break;
         }
     },
     getState: function () {
         return this._keysState;
+    },
+    getActionStack: function () {
+        return this._actionStack;
     }
 };
--- dev/js/idfapp/scene.js
+++ dev/js/idfapp/scene.js
@@ -7,21 +7,54 @@ IDFAPP.Scene = function () {
     this._camera.position = new matrix.Vector([0, 0, 30]);
 
     this._controls = new IDFAPP.Controls();
+
+    this._fractalBuilder = new IDFAPP.FractalLSystem();
+
+    this._lastFractalParams = null;
+
+    this._rebuildFractal({
+        rules: {
+            axiom: "F",
+            main: "F+[F-F]+F",
+            //main: "FF+[+F-F[F0|F0]-0]-[-F+F+F]",
+            secondary: [
+                "F-[[0]+0]+F[+|F0]-0",
+                "F+F+F+F+F+FFFFFL[22]FFFF",
+                "FFFFFF333FFF",
+                "FFFFF00FFFFF"
+            ]
+        },
+        iterations: 3,
+        theta: 22.5,
+        scale: 1,
+        angleInitial: 5
+    });
 };
 
 IDFAPP.Scene.prototype = Object.create(GLW.Scene.prototype);
 IDFAPP.Scene.prototype.constructor = IDFAPP.Scene;
+
+IDFAPP.Scene.prototype._rebuildFractal = function (params) {
+    this._lastFractalParams = params;
+
+    this.clear();
+    this.add(new IDFAPP.LineMesh(this._fractalBuilder.build(params)));
+};
 
 IDFAPP.Scene.prototype.add = function (o) {
     if (o instanceof IDFAPP.LineMesh && !this.objects.has(o))
         this.objects.add(o);
 };
 
+IDFAPP.Scene.prototype.clear = function () {
+    this.objects.clear();
+};
+
 IDFAPP.Scene.prototype.getCamera = function () {
     return this._camera;
 };
 
-IDFAPP.Scene.prototype.update = function () {
+IDFAPP.Scene.prototype._updateCamera = function () {
     var state = this._controls.getState();
 
     var x = 0, y = 0, speed = 0.5;
@@ -40,10 +73,46 @@ IDFAPP.Scene.prototype.update = function () {
 
     if (x !== 0 || y !== 0) {
         var len = Math.sqrt(x * x + y * y) / speed;
+
         x /= len;
         y /= len;
 
         this._camera.position.data[0] += x;
         this._camera.position.data[1] += y;
     }
+};
+
+IDFAPP.Scene.prototype._doFractalAction = function (action) {
+    switch (action) {
+        case "increment":
+            this._lastFractalParams.iterations++;
+            break;
+        case "decrement":
+            this._lastFractalParams.iterations--;
+            break;
+    }
+
+    this._lastFractalParams.iterations = Math.max(Math.min(this._lastFractalParams.iterations, 7), 0);
+    this._rebuildFractal(this._lastFractalParams);
+};
+
+IDFAPP.Scene.prototype._updateActions = function () {
+    var stack = this._controls.getActionStack();
+
+    var lastFractalAction = null;
+
+    while (stack.length > 0) {
+        var action = stack.pop();
+
+        if (action.type === "fractal")
+            lastFractalAction = action;
+    }
+
+    if (lastFractalAction)
+        this._doFractalAction(action.action);
+};
+
+IDFAPP.Scene.prototype.update = function () {
+    this._updateCamera();
+    this._updateActions();
 };
